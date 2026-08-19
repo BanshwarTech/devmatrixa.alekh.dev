c42d2262efd3954187fb9358a7ffa68428189237
Alekh Banshwar <152582604+BanshwarTech@users.noreply.github.com>
2026-08-19 11:38:12 +0530

    update favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -27,7 +27,7 @@
     <meta name="twitter:title" content="{{ $ogTitle ?? ($title ?? 'Devmatrixa') }}">
     <meta name="twitter:description" content="{{ $ogDescription ?? ($description ?? config('seo.site_description')) }}">
 
-    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" href="{{ asset('favicon.ico?v=2') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
